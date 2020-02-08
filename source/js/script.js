5a102d7b3e763d9845b0e82a855e1ea3e01638ba
SoundM <galina45192@gmail.com>
2020-02-08 04:08:07 +0100

    Тренеры со слайдером

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,187 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+// var pageHeader = document.querySelector('.page-header');
+// var headerToggle = document.querySelector('.page-header__toggle');
+//
+// pageHeader.classList.remove('page-header--nojs');
+//
+// headerToggle.addEventListener('click', function () {
+//   if (pageHeader.classList.contains('page-header--closed')) {
+//     pageHeader.classList.remove('page-header--closed');
+//     pageHeader.classList.add('page-header--opened');
+//   } else {
+//     pageHeader.classList.add('page-header--closed');
+//     pageHeader.classList.remove('page-header--opened');
+//   }
+// });
 
-pageHeader.classList.remove('page-header--nojs');
+// Якорь
+var yak1 = document.querySelector('a[href="#yak1"]');
+var place1 = document.getElementById('#yak1');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
+var goHref = function (place) {
+  place.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  });
+};
+
+yak1.addEventListener('click', function () {
+  goHref(place1);
 });
+
+
+// слайдер
+var allTrainers = document.querySelector('.trainers__menu');
+allTrainers.classList.remove('no-js');
+
+var multiItemSlider = (function () {
+  return function (selector) {
+    var mainElement = document.querySelector(selector); // основный элемент блока
+    var sliderSection = document.querySelector('.trainers');
+    var sliderMenu = mainElement.querySelector('.trainers__menu'); // обертка для .trainers-item
+    var sliderItems = mainElement.querySelectorAll('.trainers__item'); // элементы (.trainers-item)
+    var sliderControls = mainElement.querySelectorAll('.trainers__control'); // элементы управления
+    var sliderControlLeft = mainElement.querySelector('.trainers__control--left'); // кнопка "LEFT"
+    var sliderControlRight = mainElement.querySelector('.trainers__control--right'); // кнопка "RIGHT"
+    var sectionWidth = parseFloat(getComputedStyle(sliderSection).width);
+    var menuWidth = parseFloat(getComputedStyle(sliderMenu).width); // ширина обёртки
+    var itemWidth = parseFloat(getComputedStyle(sliderItems[0]).width); // ширина одного элемента
+    var positionLeftItem = 0; // позиция левого активного элемента
+    var transform = 0; // значение транфсофрмации .trainers__menu
+    var step = itemWidth / menuWidth * 100; // величина шага (для трансформации)
+    var items = []; // массив элементов
+    // наполнение массива items
+    sliderItems.forEach(function (item, index) {
+      items.push({item: item, position: index, transform: 0});
+    });
+
+    var position = {
+      getMin: 0,
+      getMax: items.length - 1,
+    };
+
+    var transformItem = function (direction) {
+      if (direction === 'right') {
+        if ((positionLeftItem + menuWidth / itemWidth - 1) >= position.getMax) {
+          return;
+        }
+        if (!sliderControlLeft.classList.contains('trainers__control--show')) {
+          sliderControlLeft.classList.add('trainers__control--show');
+        }
+        if (sliderControlRight.classList.contains('trainers__control--show') && (positionLeftItem + menuWidth / itemWidth) >= position.getMax) {
+          sliderControlRight.classList.remove('trainers__control--show');
+        }
+        positionLeftItem++;
+        transform -= step;
+      }
+      if (direction === 'left') {
+        if (positionLeftItem <= position.getMin) {
+          return;
+        }
+        if (!sliderControlRight.classList.contains('trainers__control--show')) {
+          sliderControlRight.classList.add('trainers__control--show');
+        }
+        if (sliderControlLeft.classList.contains('trainers__control--show') && positionLeftItem - 1 <= position.getMin) {
+          sliderControlLeft.classList.remove('trainers__control--show');
+        }
+        positionLeftItem--;
+        transform += step;
+      }
+      sliderMenu.style.transform = 'translateX(' + transform + '%)';
+    };
+
+    // обработчик события click для кнопок "назад" и "вперед"
+    var controlClick = function (e) {
+      if (e.target.classList.contains('trainers__control')) {
+        e.preventDefault();
+        var direction = e.target.classList.contains('trainers__control--right') ? 'right' : 'left';
+        if (sectionWidth >= 1200) {
+          transformItem(direction);
+          transformItem(direction);
+          transformItem(direction);
+          transformItem(direction);
+        }
+        if (sectionWidth >= 768) {
+          transformItem(direction);
+          transformItem(direction);
+        }
+        if (sectionWidth >= 320) {
+          transformItem(direction);
+        }
+      }
+    };
+
+    var setUpListeners = function () {
+      // добавление к кнопкам "назад" и "вперед" обрботчика controlClick для событя click
+      sliderControls.forEach(function (item) {
+        item.addEventListener('click', controlClick);
+      });
+    };
+
+    // инициализация
+    setUpListeners();
+
+    return {
+      right: function () { // метод right
+        transformItem('right');
+      },
+      left: function () { // метод left
+        transformItem('left');
+      }
+    };
+  };
+}());
+
+var trainers = multiItemSlider('.trainers');
+
+// слайдер легкий
+// var slideIndex = 1,
+//   slides = document.querySelectorAll('.slider-item'),
+//   prev = document.querySelector('.prev'),
+//   next = document.querySelector('.next'),
+//   dotsWrap = document.querySelector('.slider-dots'),
+//   dots = document.querySelectorAll('.dot');
+//
+// showSlides(slideIndex);
+//
+// function showSlides(n) {
+//
+//   if (n > slides.length) {
+//     slideIndex = 1;
+//   }
+//   if (n < 1) {
+//     slideIndex = slides.length;
+//   }
+//
+//   slides.forEach((item) => item.style.display = 'none');
+//   // for (let i = 0; i < slides.length; i++) {
+//   //     slides[i].style.display = 'none';
+//   // }
+//   dots.forEach((item) => item.classList.remove('dot-active'));
+//
+//   slides[slideIndex - 1].style.display = 'block';
+//   dots[slideIndex - 1].classList.add('dot-active');
+// }
+//
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+//
+// prev.addEventListener('click', function() {
+//   plusSlides(-1);
+// });
+//
+// next.addEventListener('click', function() {
+//   plusSlides(1);
+// });
+//
+// dotsWrap.addEventListener('click', function(event) {
+//   for (let i = 0; i < dots.length + 1; i++) {
+//     if (event.target.classList.contains('dot') && event.target == dots[i-1]) {
+//       currentSlide(i);
+//     }
+//   }
+// });
